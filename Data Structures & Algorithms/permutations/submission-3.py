9270261ccd6d740c48eb1cf8e08ae8af2256f34f
Yonas650 <111200835+Yonas650@users.noreply.github.com>
2026-06-19 09:53:12 -0400

    Add: permutations - submission-3

---
+++ Data Structures & Algorithms/permutations/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        path, res = [],[]
+        used = [False]*len(nums)
+
+        def backtrack():
+            if len(path)==len(nums):
+                res.append(path.copy())
+                return
+            for i in range(len(nums)):
+                if used[i]:
+                    continue
+                used[i]=True
+                path.append(nums[i])
+                backtrack()
+                path.pop()
+                used[i]=False
+        backtrack()
+        return res
