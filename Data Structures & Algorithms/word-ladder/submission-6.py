cb7e27551e4c658ce25f0af6a97503cf4e68bf02
Yonas650 <111200835+Yonas650@users.noreply.github.com>
2026-06-30 10:35:51 -0400

    Add: word-ladder - submission-6

---
+++ Data Structures & Algorithms/word-ladder/submission-6.py
@@ -0,0 +1,35 @@
+import string
+
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        lowercase= list(string.ascii_lowercase)
+        wordSet = set(wordList)
+        def generate(word):
+            neighbors=[]
+            for i in range(len(word)):
+                nei=''
+                for ch in lowercase:
+                    if ch != word[i]:
+                        nei=word[:i]+ch+word[i+1:]
+                        neighbors.append(nei)
+            return neighbors
+
+        queue = deque([(beginWord, 1)])
+        visited= set([beginWord])
+        
+        while queue:
+
+            word, dist= queue.popleft()
+
+            if word==endWord:
+                return dist
+
+            neighbors= generate(word)
+
+            for nei in neighbors:
+                if nei not in visited and nei in wordSet:
+                    visited.add(nei)
+                    queue.append((nei,dist+1))
+                
+        return 0
+
